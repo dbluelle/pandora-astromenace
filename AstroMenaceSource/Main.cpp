302baf1acebd8416a72f13ebc9d7a59704449c4f
Ludger Krämer <dbluelle@blau-weissoedingen.de>
2013-02-03 17:11:41 +0100

    enable neon-optimized math

--- AstroMenaceSource/Main.cpp
+++ AstroMenaceSource/Main.cpp
@@ -134,6 +134,22 @@ void* SDL_GL_GetProcAddress(const char* name)
 
 
 
+#ifdef PANDORA
+void enable_fastmath()
+{
+	static const unsigned int x = 0x04086060;
+	static const unsigned int y = 0x03000000;
+	int r;
+	asm volatile (
+		"fmrx	%0, fpscr			\n\t"	//r0 = FPSCR
+		"and	%0, %0, %1			\n\t"	//r0 = r0 & 0x04086060
+		"orr	%0, %0, %2			\n\t"	//r0 = r0 | 0x03000000
+		"fmxr	fpscr, %0			\n\t"	//FPSCR = r0
+		: "=r"(r)
+		: "r"(x), "r"(y)
+	);
+}
+#endif
 
 
 
@@ -145,6 +161,9 @@ void* SDL_GL_GetProcAddress(const char* name)
 //------------------------------------------------------------------------------------
 int main( int argc, char **argv )
 {
+#ifdef PANDORA
+	enable_fastmath();
+#endif
 
 #ifdef WIN32
 	// иним пути для винды
